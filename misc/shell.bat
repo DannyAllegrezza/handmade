02d5cf322225042a7e0f1790ed9b360f02ccae8d
danny allegrezza <danny@dannyallegrezza.com>
2017-01-05 21:47:40 -0500

    Day 1
    * Setup environment for building our game
    * Create a batch file which will build our app (build.bat)
    * Display a window on the screen
    * Create main cpp file, create main entry point in our app. See MSDN on
    WinMain for more details:
    https://msdn.microsoft.com/en-us/library/windows/desktop/ms633559(v=vs.85).aspx

---
+++ misc/shell.bat
@@ -0,0 +1,8 @@
+@echo off
+
+REM
+REM  To run this at startup, use this as your shortcut target:
+REM  %windir%\system32\cmd.exe /k w:\handmade\misc\shell.bat
+REM
+
+set path=w:\handmade\misc;%path%
