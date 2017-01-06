c009f8e19d906e7b45a5cc4d02e10d284b781a39
danny allegrezza <danny@dannyallegrezza.com>
2017-01-06 12:31:59 -0500

    Create main loop, handle WIN messages
    Demo how we can handle different Messages passed back to us from
    Windows. Paint the screen black and then white based as the screen is
    resized

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,12 +1,129 @@
 #include <windows.h>
 
-int CALLBACK WinMain(
-    HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nCmdShow)
+LRESULT CALLBACK MainWindowCallback(HWND Window,
+	UINT Message,
+	WPARAM WParam,
+	LPARAM LParam)
 {
-    WNDCLASS WindowClass = {};
+	LRESULT Result = 0;
+
+	switch (Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugString("WM_SIZE\n");
+		} break;
+
+		case WM_DESTROY:
+		{
+			OutputDebugString("WM_DESTROY\n");
+		} break;
+		
+		case WM_CLOSE:
+		{
+			OutputDebugString("WM_CLOSE\n");
+		} break;
+
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugString("WM_ACTIVATEAPP\n");
+		} break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.right;
+			LONG Width = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			LONG Height = Paint.rcPaint.right - Paint.rcPaint.left;
+			
+			static DWORD Operation = WHITENESS;
+			PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+			
+			if (Operation == WHITENESS)
+			{
+				Operation = BLACKNESS;
+			}
+			else
+			{
+				Operation = WHITENESS;
+			}
+
+			EndPaint(Window, &Paint);
+
+		} break;
+
+		default:
+		{
+			//OutputDebugString("Default message\n\n");
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+	}
+
+	return (Result);
+}
+
+int CALLBACK WinMain(
+    HINSTANCE Instance,
+    HINSTANCE PrevInstance,
+    LPSTR CommandLine,
+    int ShowCode)
+{
+    WNDCLASS WindowClass = {}; // "ZII" - Zero is initialization 
     
+	//TODO(danny): Check if HREDRAW/VREDRAW/OWNDC still matter?
+	WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+	
+	if (RegisterClass(&WindowClass)) 
+	{
+		HWND WindowHandle = 
+			CreateWindowEx(
+				0, 
+				WindowClass.lpszClassName, 
+				"Handmade Hero", 
+				WS_OVERLAPPEDWINDOW | WS_VISIBLE, 
+				CW_USEDEFAULT, 
+				CW_USEDEFAULT, 
+				CW_USEDEFAULT, 
+				CW_USEDEFAULT, 
+				0, 
+				0, 
+				Instance, 
+				0);
+
+		if (WindowHandle)
+		{
+			for (;;)
+			{
+				MSG Message;
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if (MessageResult > 0)
+				{
+					// Translate and dispatch the message
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+			
+		}
+		else 
+		{
+
+		}
+	}
+	else
+	{
+		//TODO(danny): add logging
+	}
+
     return (0);
 }
